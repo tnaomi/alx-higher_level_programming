9526728fd374ccb37b3624bc2df45c34990bb525
Tadala K <naomikapengule@gmail.com>
2024-03-14 08:06:33 -0700

    USed forEach loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const myVar = 'JavaScript is amazing';
+const myPy = 'Python is cool';
+const myC = 'C is fun';
+const myLangs = [myC, myPy, myVar];
+
+myLangs.forEach((val) => {
+  console.log(`${val}`);
+});
