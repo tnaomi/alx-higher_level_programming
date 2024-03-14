084334f16db9fcc6007b7b906705b4b0fea5211b
Tadala K <naomikapengule@gmail.com>
2024-03-14 06:29:29 -0700

    Print a single_string

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,3 @@
 #!/usr/bin/node
 
-const myVar = 'JavaScript is amazing';
-const myPy = 'Python is cool';
-const myC = 'C is fun';
-
-console.log(myC, '\n', myPy, '\n', myVar);
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
