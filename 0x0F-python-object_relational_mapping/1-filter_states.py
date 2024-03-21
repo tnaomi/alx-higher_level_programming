2e85c85f7a6a2aeba61bf2a144531baaea9c5ca2
Tadala K <naomikapengule@gmail.com>
2024-03-21 09:56:12 -0700

    Corrections

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -14,7 +14,13 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     curs = db.cursor()
-    curs.execute("""SELECT * FROM states
-                WHERE name LIKE BINARY '{}'
-                ORDER BY states.id ASC""".format(sys.argv[4]).strip("'"))
-    [print(state) for state in curs.fetchall()]
+    cursor.execute("SELECT * \
+                    FROM states \
+                    WHERE CONVERT(`name` USING Latin1) \
+                    COLLATE Latin1_General_CS \
+                    LIKE 'N%';")
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    db.close()
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,20 +7,20 @@ import MySQLdb
 import sys
 
 if __name__ == '__main__':
+
     db = MySQLdb.connect(
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3],
         port=3306,
         host='localhost')
+command = 'SELECT * from states WHERE name = %s ORDER BY states.id'
+curs = db.cursor()
+curs.execute(command, (sys.argv[4], ))
 
-    curs = db.cursor()
-    curs.execute('SELECT * from states WHERE name = %s ORDER BY states.id',
-                   (sys.argv[4], ))
+states = curs.fetchall()
+for state in states:
+    print(state)
 
-    states = curs.fetchall()
-    for state in states:
-        print(state)
-
-    curs.close()
-    db.close()
+curs.close()
+db.close()
