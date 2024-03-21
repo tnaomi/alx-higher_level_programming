79b572e7f38fda43d6b951d1346d876017f680a6
Tadala K <naomikapengule@gmail.com>
2024-03-21 11:18:53 -0700

    Fixed Variable Errors

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -14,13 +14,13 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     curs = db.cursor()
-    cursor.execute("SELECT * \
+    curs.execute("SELECT * \
                     FROM states \
                     WHERE CONVERT(`name` USING Latin1) \
                     COLLATE Latin1_General_CS \
                     LIKE 'N%';")
-    states = cursor.fetchall()
+    states = curs.fetchall()
     for state in states:
         print(state)
-    cursor.close()
+    curs.close()
     db.close()
