be3e461b5e3a8af4e3cb6a2560c95e58388b3141
Tadala K <naomikapengule@gmail.com>
2024-03-22 18:46:01 -0700

    Display first state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+print the first State
+'''
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+
+
+if __name__ == '__main__':
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]))
+    Instance = sessionmaker(bind=engine)
+    session = Instance()
+
+    state = session.query(State).order_by(State.id).first()
+    if state:
+        print('{}: {}'.format(state.id, state.name))
+    else:
+        print('Nothing')
+    session.close()
+
