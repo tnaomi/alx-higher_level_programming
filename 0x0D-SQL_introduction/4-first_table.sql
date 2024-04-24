e4ba72858e2488e8c241a7679c59cb45cdab7e09
Tadala <naomikapengule@gmail.com>
2024-04-24 21:05:43 +0200

    Mandatory Tasks: removed USE db

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,5 @@
 -- Create a Database, script should not fail if it already exists
 
-USE hbtn_0c_0;
 CREATE TABLE IF NOT EXISTS first_table(
 	id INT ,
 	name VARCHAR(256)
--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
 -- Display table info w/o using DESCRIBE, EXPLAIN
 
-USE hbtn_0c_0;
+
 SHOW COLUMNS FROM first_table;
