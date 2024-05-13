7379289e6e55037d5cc6bec2d44fc5cef0749d4e
Tadala <naomikapengule@gmail.com>
2024-05-13 12:59:28 +0200

    Add condition for a single arg

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,6 +6,9 @@ if __name__ == "__main__":
     if its == 1:
         print("0 arguments.")
     else:
-        print("{} arguments:".format(its - 1))
+        args = "arguments"
+        if its == 2:
+            args = args.strip('s')
+        print("{} {}:".format((its - 1), args))
         for it in range(1, (its)):
             print("{}: {}".format(it, argv[it]))
