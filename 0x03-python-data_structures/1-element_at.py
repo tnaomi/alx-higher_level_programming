6b3f50631f5ba2650de36b0a2b3c625fb98f4efd
Tadala <naomikapengule@gmail.com>
2024-05-15 10:10:15 +0200

    Re-ordered cond statements

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if idx < 0 | idx > len(my_list):
-        return None
-    for i in range(len(my_list)):
-        while idx != i:
-            i += 1
-            pass
-        return my_list[i]
+    """### Find an element at a given index
+    Args:
+        my_list (_list_): _Input array of integers_
+        idx (_int_): _Index of the list_
+
+    Returns:
+        list or None: list if idx w/in range and None if error
+    """
+    if idx in range(len(my_list)):
+        return my_list[idx]
+    return None
