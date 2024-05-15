761e1aaadfee02c20ececb0da2e30dbd7f6b98a7
Tadala <naomikapengule@gmail.com>
2024-05-15 10:14:34 +0200

    Added None conditional

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,8 +2,9 @@
 def print_reversed_list_integer(my_list=[]):
     """### Print a list of integers in reversed order
 
-    Args:
-        my_list (list, optional): An array of elements. Defaults to [].
+    #### Args:
+        my_list (_list_, _optional_): An array of elements. Defaults to [].
     """
-    for indx in range((len(my_list) - 1), -1, -1):
-        print("{:d}".format(my_list[indx]))
+    if my_list is not None:
+        for indx in range((len(my_list) - 1), -1, -1):
+            print("{:d}".format(my_list[indx]))
