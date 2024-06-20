6ba0c69b45e40b9f25ac36a7d47edaa0f029010b
Tadala <naomikapengule@gmail.com>
2024-06-20 17:49:53 +0000

    Rewrote if statements

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -14,6 +14,8 @@ class BaseGeometry():
         """ Validates `value`. `name` assumed as string """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-        return value
+        else:
+            if value <= 0:
+                raise ValueError("{} must be greater than 0".format(name))
+            else:
+                return value
