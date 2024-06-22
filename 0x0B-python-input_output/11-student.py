9caa2339ba642c603341113658c5d1f875dfba18
Tadala <naomikapengule@gmail.com>
2024-06-22 05:11:34 +0000

    Rewrote dict comprehension

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -6,32 +6,18 @@ class Student():
     """ Defines class `Student` """
     def __init__(self, first_name, last_name, age):
         """ Initialises `Student` w `first_name`, `last_name`, `age` """
-        if type(first_name) is not str:
-            raise TypeError("first name must be a string")
-        elif type(last_name) is not str:
-            raise TypeError("last name must be a string")
-        elif type(age) is not int:
-            raise TypeError("age must be an integer")
-        if age < 0:
-            raise ValueError("age must be a positive number")
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
         """ Returns a dictionary represantation of `Student` instance """
-        if attrs is None:
-            return self.__dict__
+        if not isinstance(attrs, (list)) or not all(isinstance(i, str) 
+                                                    for i in attrs):
+            return self.__dict__.copy()
         else:
-            attributes, values, new_dict = attrs.copy(), [], {}
-            for i in attributes:
-                if i not in self.__dict__:
-                    attributes.remove(i)
-            values.extend(self.__dict__.get(i) for i in attributes)
-            for i in range(len(attributes)):
-                new_dict.__setitem__(attributes[i], values[i])
-            self.__dict__ = new_dict.copy()
-            return self.__dict__
+            new_d = {i: self.__dict__[i] for i in attrs if i in self.__dict__}            
+            return new_d
 
     def reload_from_json(self, json):
         """ Replaces an instance w JSON data """
