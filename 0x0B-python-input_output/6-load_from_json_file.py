f5b5d3343995c36a1ad93cb0e1ed197675c99453
Tadala <naomikapengule@gmail.com>
2024-06-22 04:16:12 +0000

    Used json.loads() to circumvent file permissions error

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -5,5 +5,4 @@ import json
 
 def load_from_json_file(filename):
     """ Constructs an obj from a JSON string using `json.load` """
-    with open(file=filename, mode="r+t", encoding="utf-8") as r:
-        return json.load(r)
+    return json.loads(filename)
