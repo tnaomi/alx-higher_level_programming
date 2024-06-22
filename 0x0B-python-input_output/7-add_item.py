d9cb9aed7e9021312c70852cc79163f375d3c3d4
Tadala <naomikapengule@gmail.com>
2024-06-22 04:35:00 +0000

    Added path.isfile() check to verify

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,12 +6,17 @@ Contains function `save_to_json_file` & `load_from_json_file`
 
 if __name__ == "__main__":
     from sys import argv
+    from os import path
     j_save = __import__('5-save_to_json_file').save_to_json_file
     j_load = __import__('6-load_from_json_file').load_from_json_file
 
-    items = j_load("add_item.json")
+    file = "add_item.json"
+    if path.isfile(file):
+        items = j_load(file)
+    else:
+        items = []
     if len(argv) < 2:
         items.append("")
     else:
         items.extend(argv[1:len(argv)])
-    j_save(items, "add_item.json")
+    j_save(items, file)
